cea05b2197a13f7325fb01d6cbc52491d9bc91b7
Lewin Kelly <heli@odex.cf>
2024-11-04 23:36:32 +0000

    Test multiple files with different optimisation levels

--- main_test.go
+++ main_test.go
@@ -2,28 +2,58 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	fmt.Println("Compiling")
+func filename(f string) string {
+	return fmt.Sprintf("test/%s", f)
+}
 
-	// execute luau-compile
-	cmd := exec.Command("luau-compile", "--binary", "-O0", "main.luau")
-	// get the output
+func luauPrint(a any) string {
+	switch v := a.(type) {
+	case string:
+		return v
+	case float64:
+		if math.Pow10(19) <= v && v < math.Pow10(21) {
+			return fmt.Sprintf("%.0f", v)
+		} else if float64(int(v)) == v && v < math.Pow10(21) {
+			return fmt.Sprintf("%d", int(v))
+		} else if v == math.Inf(1) {
+			return "inf"
+		} else if v == math.Inf(-1) {
+			return "-inf"
+		} else if v > math.Pow10(50) {
+			return fmt.Sprintf("%.0e", v)
+		}
+		return fmt.Sprint(v)
+	}
+	return fmt.Sprint(a)
+}
+
+func litecode(t *testing.T, f string, o int) string {
+	cmd := exec.Command("luau-compile", "--binary", fmt.Sprintf("-O%d", o), filename(f))
 	bytecode, err := cmd.Output()
 	if err != nil {
 		t.Error("error running luau-compile:", err)
+		return ""
 	}
 
 	deserialised := luau_deserialise(bytecode)
 
-	output1 := strings.Builder{}
+	b := strings.Builder{}
 	luau_print := func(args ...any) (ret []any) {
-		output1.WriteString(fmt.Sprint(args...))
-		output1.WriteString("\r\n")
+		// b.WriteString(fmt.Sprint(args...))
+		for i, arg := range args {
+			b.WriteString(luauPrint(arg))
+			if i < len(args)-1 {
+				b.WriteString("\t")
+			}
+		}
+		b.WriteString("\r\n") // yeah
 		return
 	}
 
@@ -32,18 +62,53 @@ func TestHello(t *testing.T) {
 	})
 	fn()
 
-	fmt.Println()
+	return b.String()
+}
 
-	cmd2 := exec.Command("luau", "main.luau")
-	output2, err := cmd2.Output()
+func luau(f string) (string, error) {
+	cmd := exec.Command("luau", filename(f))
+	o, err := cmd.Output()
 	if err != nil {
-		t.Error("error running luau:", err)
+		return "", err
 	}
 
-	result1 :=  output1.String()
-	result2 := string(output2)
+	return string(o), nil
+}
 
-	if result1 != result2 {
-		t.Errorf("output mismatch:\n%s\n%s", result1, result2)
+func TestConformance(t *testing.T) {
+	files, err := os.ReadDir("test")
+	if err != nil {
+		t.Error("error reading test directory:", err)
+		return
 	}
+
+	for _, f := range files {
+		name := f.Name()
+		fmt.Println(" -- Testing", name, "--")
+
+		og, err := luau(name)
+		if err != nil {
+			t.Error("error running luau:", err)
+			return
+		}
+
+		outputs := []string{
+			litecode(t, name, 0),
+			litecode(t, name, 1),
+			litecode(t, name, 2),
+		}
+		fmt.Println()
+
+		for _, o := range outputs {
+			if o != og {
+				t.Errorf("output mismatch:\n-- Expected\n%s\n-- Got\n%s", og, o)
+				return
+			}
+		}
+
+		fmt.Println(og)
+	}
+
+	fmt.Println("-- Done! --")
+	fmt.Println()
 }
