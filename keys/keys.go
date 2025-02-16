49108d9c013dc49d4f1f83fde9db62cc88a7bc70
Lewin Kelly <heli@odex.cf>
2025-02-16 20:21:20 +0000

    Keypair creation, signatures & verification, and testing

--- keys/keys.go
+++ keys/keys.go
@@ -2,14 +2,15 @@ package keys
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/ChainSafe/go-schnorrkel"
 )
 
 type Keypair struct {
-	pk PK
-	sk SK
+	Pk  PK
+	Sk  SK
+	spk *schnorrkel.PublicKey
+	ssk *schnorrkel.SecretKey
 }
 
 func GenerateKeys(threads uint8, stop <-chan struct{}) Keypair {
@@ -19,19 +20,16 @@ func GenerateKeys(threads uint8, stop <-chan struct{}) Keypair {
 		go func() {
 			for {
 				// 🔥🔥 HOT PATH 🔥🔥
+				// ~45000 hashes/core/s on my machine, about 6 minutes (1 rap god) to find a keypair with 1 thread
 				skm, _ := schnorrkel.GenerateMiniSecretKey()
-				sk := skm.ExpandEd25519().Encode()
 
-				pk, err := SKtoPK(sk)
+				kp, err := keypair(skm.ExpandEd25519().Encode())
 				if err != nil {
 					continue
 				}
 
-				fmt.Println("found keypair")
-
 				select {
-				case found <- Keypair{pk, sk}:
-					fmt.Println("sent keypair")
+				case found <- kp:
 				case <-stop:
 					return
 				}
@@ -42,19 +40,55 @@ func GenerateKeys(threads uint8, stop <-chan struct{}) Keypair {
 	return <-found
 }
 
-func SKtoPK(skBytes SK) (PK, error) {
-	sk := schnorrkel.NewSecretKey(skBytes, [32]byte{})
+func keypair(s SK) (kp Keypair, err error) {
+	sk := schnorrkel.NewSecretKey(s, [32]byte{}) // nonce unused
 
 	pk, err := sk.Public()
 	if err != nil {
-		return PK{}, err
+		return
 	}
 
 	pkb := pk.Encode()
 	if pkb[0] != 0 || pkb[1] != 0 || pkb[2] != 0 {
 		// not enough pow
-		return PK{}, errors.New("private key maps to an invalid public key")
+		return Keypair{}, errors.New("private key maps to an invalid public key")
 	}
 
-	return PK(pkb[3:]), nil
+	return Keypair{PK(pkb[3:]), s, pk, sk}, nil
+}
+
+var domain = []byte("coputer")
+
+func (kp Keypair) Sign(msg []byte) (s [64]byte, err error) {
+	ctx := schnorrkel.NewSigningContext(domain, msg)
+
+	sig, err := kp.ssk.Sign(ctx)
+	if err != nil {
+		return
+	}
+
+	return sig.Encode(), nil
+}
+
+func (pk PK) Verify(msg []byte, sig [64]byte) (verified bool, err error) {
+	ctx := schnorrkel.NewSigningContext(domain, msg)
+
+	pkb := [32]byte{}
+	copy(pkb[3:], pk[:])
+
+	spk := &schnorrkel.PublicKey{}
+	spk.Decode(pkb)
+
+	s := &schnorrkel.Signature{}
+	s.Decode(sig)
+
+	return spk.Verify(s, ctx)
+}
+
+func (kp Keypair) Decrypt(ct []byte) (msg []byte, err error) {
+	return
+}
+
+func (pk PK) Encrypt(msg []byte)(ct []byte, err error) {
+	return
 }
--- keys/keys_test.go
+++ keys/keys_test.go
@@ -8,41 +8,56 @@ import (
 func TestKeys(t *testing.T) {
 	fmt.Println("starting")
 
-	kp := GenerateKeys(6, nil)
+	kp := GenerateKeys(1, nil)
 
 	fmt.Println()
-	fmt.Println(kp.pk)
-	pkf := kp.pk.Encode()
-	skf := kp.sk.Encode()
+	fmt.Println(kp.Pk)
+	pkf := kp.Pk.Encode()
+	skf := kp.Sk.Encode()
 	fmt.Println(pkf)
 	fmt.Println(skf)
 	fmt.Println()
 }
 
 func TestEncode(t *testing.T) {
-	const pkf = "copub:4wjzd3p8o-fso3bbdm9-aecqtu6zz-vesozdnna-zbcdeuo9r"
+	// const pkf = "copub:4wjzd3p8o-fso3bbdm9-aecqtu6zz-vesozdnna-zbcdeuo9r"
 	const skf = "cosec:4eh0gemdyr-rqvuo9ijxc-ahzegh6881-taulhavmh4-lo1ziwy3v2"
-
-	pk, err := DecodePK(pkf)
-	if err != nil {
-		panic(err)
-	}
 
 	sk, err := DecodeSK(skf)
 	if err != nil {
 		panic(err)
 	}
 
-	pk2, err := SKtoPK(sk)
+	kp, err := keypair(sk)
 	if err != nil {
 		panic(err)
 	}
 
+	pk := kp.Pk
 	fmt.Println(pk)
-	fmt.Println(pk2)
 
-	fmt.Println(pk.Encode())
+	pkf := pk.Encode()
+
 	fmt.Println(pkf)
-	fmt.Println(sk.Encode())
 	fmt.Println(skf)
+	fmt.Println()
+
+	const message = "This is a message!"
+
+	sig, err := kp.Sign([]byte(message))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(sig)
+
+
+	ver, err := pk.Verify([]byte(message), sig)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(ver)
+
+	fmt.Println()
 }
