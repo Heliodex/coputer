5ec13a76f177138ebf15bdf6454464f5768ea323
Lewin Kelly <heli@odex.cf>
2025-02-25 12:24:01 +0000

    Improve compression with gzip filenames, remove gzip headers

--- exec/exec.go
+++ exec/exec.go
@@ -9,74 +9,72 @@ import (
 	"strings"
 )
 
-// var header = []byte{0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00}
-
-func Compress(f []byte) (c []byte, err error) {
-	var b bytes.Buffer
-	w, err := gzip.NewWriterLevel(&b, gzip.BestCompression)
-	if err != nil {
-		return
-	}
-
-	w.Write(f)
-
-	w.Close()
-	return b.Bytes(), nil // remove the 3 byte header
-}
-
-func Decompress(f []byte) (c []byte, err error) {
-	// add the 3 byte header
-	// f = append(header, f...)
-
-	var b bytes.Buffer
-	r, err := gzip.NewReader(bytes.NewReader(f))
-	if err != nil {
-		return
-	}
-
-	b.ReadFrom(r)
-
-	r.Close()
-	return b.Bytes(), nil
-}
+var gzheader = []byte{0x1f, 0x8b, 0x08, 0x08, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff}
 
 type File struct {
 	path string
 	data []byte
 }
 
+func Compress(n string, f []byte) (cf File, err error) {
+	var b bytes.Buffer
+	w := gzip.NewWriter(&b)
+
+	w.Name = n
+	if _, err = w.Write(f); err != nil {
+		return
+	} else if err = w.Close(); err != nil {
+		return
+	}
+
+	return File{n, b.Bytes()[len(gzheader):]}, nil // remove the header
+}
+
+func Decompress(c []byte) (f File, err error) {
+	// add the header
+	c = append(gzheader, c...)
+
+	var b bytes.Buffer
+	r, err := gzip.NewReader(bytes.NewReader(c))
+	if err != nil {
+		return
+	} else if _, err = b.ReadFrom(r); err != nil {
+		return
+	} else if err = r.Close(); err != nil {
+		return
+	}
+
+	return File{r.Name, b.Bytes()}, nil
+}
+
+func bundleFile(p string) (bf File, err error) {
+	// read
+	f, err := os.ReadFile(p)
+	if err != nil {
+		return
+	}
+
+	// remove walked directory from p (everything before first /)
+	// warning: can't use path as it is longer than p
+	s := strings.Split(p, string(filepath.Separator))
+	np := strings.Join(s[1:], "/")
+
+	return Compress(np, f)
+}
+
 func Bundle(path, entrypoint string) (b []byte, err error) {
-	var compressedFiles []File
+	var cFiles []File
 
 	// walk through the directory
-	if err = filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
-		if err != nil {
+	if err = filepath.WalkDir(path, func(p string, info os.DirEntry, err error) error {
+		if err != nil || info.IsDir() {
 			return err
-		} else if info.IsDir() {
-			return nil
-		}
-
-		// read
-		f, err := os.ReadFile(p)
-		if err != nil {
+		} else if bf, err := bundleFile(p); err != nil {
 			return err
-		}
-
-		// remove walked directory from p (everything before first /)
-		// warning: can't use path as it is longer than p
-		s := strings.Split(p, string(filepath.Separator))
-		p = strings.Join(s[1:], "/")
-
-		c, err := Compress(f)
-		if err != nil {
-			return err
-		}
-
-		// enrypoint goes first
-		if p == entrypoint {
-			compressedFiles = append([]File{{p, c}}, compressedFiles...)
+		} else if bf.path == entrypoint {
+			cFiles = append([]File{bf}, cFiles...) // entrypoint goes first
 		} else {
-			compressedFiles = append(compressedFiles, File{p, c})
+			cFiles = append(cFiles, bf)
 		}
 
 		return nil
@@ -85,11 +83,7 @@ func Bundle(path, entrypoint string) (b []byte, err error) {
 	}
 
 	// write compressed files
-	for _, c := range compressedFiles {
-		// add file path length as uint8
-		b = append(b, byte(len(c.path)))
-		b = append(b, c.path...)
-
+	for _, c := range cFiles {
 		// add file length as uvarint
 		b = binary.AppendUvarint(b, uint64(len(c.data)))
 		b = append(b, c.data...)
@@ -100,28 +94,20 @@ func Bundle(path, entrypoint string) (b []byte, err error) {
 
 func Unbundle(b []byte) (fs []File, err error) {
 	for i, lb := uint64(0), uint64(len(b)); i < lb; {
-		// read file path length
-		l := uint64(b[i])
-		i++
-
-		// read file path
-		p := b[i:][:l]
-		i += l
-
 		// read file length
 		l, n := binary.Uvarint(b[i:])
 		i += uint64(n)
 
 		// read file
-		f := b[i:][:l]
+		c := b[i:][:l]
 		i += l
 
-		dec, err := Decompress(f)
+		f, err := Decompress(c)
 		if err != nil {
 			return nil, err
 		}
 
-		fs = append(fs, File{string(p), dec})
+		fs = append(fs, f)
 	}
 
 	return
