856321358ded3f5149137eddb5f30a80a1a88b50
Lewin Kelly <heli@odex.cf>
2025-09-17 00:36:57 +0100

    Construct VSCode formatter extension template

--- ast/format/src/extension.ts
+++ ast/format/src/extension.ts
@@ -4,21 +4,26 @@ import * as vscode from "vscode"
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+function format(document: vscode.TextDocument): vscode.TextEdit[] {
+	vscode.window.showInformationMessage("Formatting document...")
+
+	const content = document.getText()
+
+	const end = document.lineAt(document.lineCount - 1)
+	const replaced = vscode.TextEdit.replace(
+		new vscode.Range(new vscode.Position(0, 0), end.range.end),
+		content.toUpperCase()
+	)
+	return [replaced]
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "coputer" is now active!')
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand(
-		"coputer.helloWorld",
-		() => {
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			vscode.window.showInformationMessage("Hello World from Coputer!")
-		}
+	vscode.window.showInformationMessage("Coputer extension is now active")
+	const disposable = vscode.languages.registerDocumentFormattingEditProvider(
+		"luau",
+		{ provideDocumentFormattingEdits: format }
 	)
 
 	context.subscriptions.push(disposable)
